867f5cc05ff5f5a5cbfeeabb603c25dfd451f042
Victor Hugo <frontdevvictor@gmail.com>
2024-06-06 08:47:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,60 @@ var cardnumber_mask = new IMask(cardnumber,{
         //     regex: '^63[7-9]\\d{0,13}',
         //     cardtype: 'instapayment'
         // },
-    ]
+        {
+            mask: '0000 000000 000000',
+            regex: '(?:2131|1800\\d{0,11}',
+            cardtype: 'jcb15'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(?:35\\d{0,2}\\d{0,12}',
+            cardtype: 'jcb'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(?:5[0678]\\d{0,2|6304|67\\d{0,2}\\d{0,12}}',
+            cardtype: 'maestro'
+        },
+        // {
+        //     mask: '0000-0000-0000-0000',
+        //     regex: '^220[0-4]\\d{0,12}',
+        //     cardtype: 'mir'
+        // },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^62\\d{0,14}',
+            cardtype: 'visa'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^62\\d{0,14}',
+            cardtype: 'unionpay'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            cardtype: 'Unknown'
+        },
+    ],
+    dispatch: function (appended, dynamicMasked) {
+        var number = (dynamicMasked.value + appended).replace(/\D/g, '');
+
+        for (var i = 0; i < dynamicMasked.compiledMasks.length; i++) {
+            let re = new RegExp(dynamicMasked.compiledMasks[i].regex);
+            if (number.match(re) != null) {
+                return dynamicMasked.compiledMasks[i];
+            }
+        }
+    }
+});
+
+//Mask the Expiration Date
+var expirationdate_mask = new IMask(expirationdate, {
+    mask: 'MM{/}YY',
+    groups: {
+        YY: new IMask.MaskedPattern.Group.Range([0, 99]),
+        MM: new IMask.MaskedPattern.Group.Range([1, 12]),
+    }
 })
 
 
